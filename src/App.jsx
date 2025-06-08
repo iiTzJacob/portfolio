409cf1af1fc35bda1df7a3609203724adaf25dcb
Jacob Colon <jj.colon8@gmail.com>
2025-06-07 20:50:48 -0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <div>
+    <div className='bg-mainbg min-h-screen'>
       <Home />
     </div>
   )
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,15 +5,15 @@ function ProjectCard() {
     return (
         <div className='grid gap-6 md:grid-cols-2'>
             {projects.map((project, index) => (
-                <div className='m-6 bg-slate-700 rounded-lg' key={index}>
+                <div className='m-6 bg-cardbg rounded-lg shadow-glow' key={index}>
                     <div className='m-2'>
-                        <div className="text-xs font-bold text-sky-500">{project.name}</div>
-                        <div className="mt-1 font-bold text-sky-700">
+
+                        <div className="mt-1 font-bold text-offwhite">
                             <a href={project.link} className='hover:underline'>
                                 {project.name}
                             </a>
                         </div>
-                    <div className="mt-2 text-sm text-gray-600">{project.description}</div>
+                    <div className="mt-2 text-sm text-offwhite">{project.description}</div>
                     </div>
 
                 </div>
